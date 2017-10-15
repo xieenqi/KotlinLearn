a9bf9f345c6b69e35e0dad50d1c7e6421c6de2e3
琦琦 <329191055@qq.com>
2017-10-15 21:17:26 +0800

    router

---
+++ routerlibrary/src/main/java/com/router/RouterActivity.kt
@@ -0,0 +1,12 @@
+package com.router
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RouterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_router)
+    }
+}
